7619cd66da8c57beb22ed58b2fda3de97e63c7b9
Luna <luna@desroches.org>
2025-11-25 10:01:46 -0500

    Initial Commit

---
+++ confessionsMaker/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  GradientMaker
+//
+//  Created by Luna DesRoches on 11/24/25.
+//
+
+import SwiftUI
+
+var colors = [
+    Color.red,
+    Color.blue,
+    Color.teal,
+    Color.mint,
+    Color.yellow,
+    Color.indigo,
+    Color.orange,
+    Color.green,
+    Color.black]
+
+
+struct ContentView: View {
+    @State var color1 = colors.randomElement()
+    @State var color2 = colors.randomElement()
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [color1!, color2!], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
+            Button("Change Color") {
+                color1 = colors.randomElement()
+                color2 = colors.randomElement()
+            }
+            .foregroundStyle(.white)
+            .padding(0)
+            .background(in: RoundedRectangle(cornerRadius: 5.0))
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ confessionsMaker/confessionsMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  confessionsMakerApp.swift
+//  confessionsMaker
+//
+//  Created by Luna DesRoches on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct confessionsMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
